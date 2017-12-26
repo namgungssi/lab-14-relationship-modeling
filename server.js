4bf23f24a596ec499d731c3ee540e7cd215c5256
dalhun namgung <bnamgung@gmail.com>
2017-12-26 13:02:46 -0800

    starting

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
 
+
+
 require ('dotenv').config ();
-require ('./lib/_server.js')
-.start (process.env.PORT);
+require ('./lib/_server').start (process.env.PORT);
 
 
 //how do I save db in .gitignore?
